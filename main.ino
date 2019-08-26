f4a27fce0075bc8abf0c44b506123b588ee5187e
Saswat Samal <39031660+saswatsamal@users.noreply.github.com>
2019-08-26 21:01:12 +0530

    Update main.ino
    Final.

--- main.ino
+++ main.ino
@@ -1,47 +1,72 @@
-#define LS 2 // left sensor
-#define RS 3 // right sensor
-#define LM1 5 // left motor M1a
-#define LM2 4 // left motor M2a
-#define RM1 7 // right motor M2a
-#define RM2 6 // right motor M2b
-void setup()
-{
-pinMode(LS, INPUT);
-pinMode(RS, INPUT);
-pinMode(LM1, OUTPUT);
-pinMode(LM2, OUTPUT);
-pinMode(RM1, OUTPUT);
-pinMode(RM2, OUTPUT);
-}
-void loop()
-{
-if(digitalRead(LS) && digitalRead(RS)) // Move Forward on line
-{
-digitalWrite(LM1, HIGH);
-digitalWrite(LM2, LOW);
-digitalWrite(RM1, HIGH);
-digitalWrite(RM2, LOW);
-}
-if(digitalRead(LS) && !(digitalRead(RS))) // turn left by rotationg left motors in forward and right ones in backward direction
-{
-digitalWrite(LM1, HIGH);
-digitalWrite(LM2, LOW);
-digitalWrite(RM1, LOW);
-digitalWrite(RM2, HIGH);
-}
-if(!(digitalRead(LS)) && digitalRead(RS)) // Turn right by rotating right motors in forward and left ones in backward direction
-{
-digitalWrite(LM1, LOW);
-digitalWrite(LM2, HIGH);
-digitalWrite(RM1, HIGH);
-digitalWrite(RM2, LOW);
+int s1=0; // all the initial values are set to zero
+int s2=0;
+int s3=0;
+int s4=0;
+int s5=0;
+int s6=0;
+int s7=0;
+int s8=0;
+
+const int lmf=10; // left motor forward
+const int lmb=11; // left motor backward
+const int rmf=12; // right motor forward
+const int rmb=13; // right motor backward
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,INPUT);// start the pin mode from 2 leave the pin 0 and 1 as this pins are RX TX you need to open the pins every time you upload a code.so its better to avoid this pins.
+pinMode(3,INPUT);
+pinMode(4,INPUT);
+pinMode(5,INPUT);
+pinMode(6,INPUT);
+pinMode(7,INPUT);
+pinMode(8,INPUT);
+pinMode(9,INPUT);
+
+pinMode(lmf,OUTPUT);
+pinMode(lmb,OUTPUT);
+pinMode(rmf,OUTPUT);
+pinMode(rmb,OUTPUT);
+Serial.begin(9600);
 }
 
-if(!(digitalRead(LS)) && !(digitalRead(RS))) // Finish line, stop both the motors
+void loop() {
+  s1=digitalRead(2);
+  s2=digitalRead(3);
+  s3=digitalRead(4);
+  s4=digitalRead(5);
+  s5=digitalRead(6);
+  s6=digitalRead(7);
+  s7=digitalRead(8);
+  s8=digitalRead(9);
+ 
+  
+  
+  // put your main code here, to run repeatedly:
+  //forward
+if((s4==0||s5==0));
+{digitalWrite(rmf, HIGH);
+digitalWrite(rmb, LOW);
+digitalWrite(lmf, HIGH);
+digitalWrite(lmb, LOW);
+delay(10);
+ }
+ //right
+ if((s6==0||s7==0||s8==0))
 {
-digitalWrite(LM1, LOW);
-digitalWrite(LM2, LOW);
-digitalWrite(RM1, LOW);
-digitalWrite(RM2, LOW);
+digitalWrite(rmf, LOW);
+digitalWrite(rmb, LOW);
+digitalWrite(lmf, HIGH);
+digitalWrite(lmb, LOW);
+delay(10);
+}
+
+//left
+if((s1==0||s2==0||s3==0))
+{
+digitalWrite(rmf, HIGH);
+digitalWrite(rmb, LOW);
+digitalWrite(lmf, LOW);
+digitalWrite(lmb, LOW);
+delay(10);
 }
 }
